d2932bbde16104ae30b5dc6359d97d43fd0de794
jiteman <jiteman@gmail.com>
2020-05-27 13:33:50 +0300

    linux build has been done

--- _tpc_cmake_settings/compiler-settings/tpc-png-lib-set-linux-compiler-settings.cmake
+++ _tpc_cmake_settings/compiler-settings/tpc-png-lib-set-linux-compiler-settings.cmake
@@ -2,15 +2,18 @@ JMSD_CMAKE_CURRENT_FILE_IN( "${JMSD_FOREIGN_COMPONENT_FULL_NAME}-linux-compiler-
 
 if ( UNIX )
 	# C
-	## set( ${PROJECT_NAME}_C_FLAGS ${CMAKE_C_FLAGS} )
+	set( ${PROJECT_NAME}_C_FLAGS ${CMAKE_C_FLAGS} )
 
 	## list( APPEND ${PROJECT_NAME}_C_FLAGS "-Wno-" ) #
+	list( APPEND ${PROJECT_NAME}_C_FLAGS "-Wno-undef" ) # "" is not defined, evaluates to 0
+	list( APPEND ${PROJECT_NAME}_C_FLAGS "-Wno-sign-conversion" ) # conversion to '' from '' may change the sign of the result
+	list( APPEND ${PROJECT_NAME}_C_FLAGS "-Wno-float-equal" ) # comparing floating-point with '' or '' is unsafe
 
-	##string( REPLACE ";" " " ${PROJECT_NAME}_C_FLAGS_STR "${${PROJECT_NAME}_C_FLAGS}" )
+	string( REPLACE ";" " " ${PROJECT_NAME}_C_FLAGS_STR "${${PROJECT_NAME}_C_FLAGS}" )
 
 	## string( REPLACE "-W" "" ${PROJECT_NAME}_C_FLAGS_STR "${${PROJECT_NAME}_C_FLAGS_STR}" ) #
 
-	## set( CMAKE_C_FLAGS ${${PROJECT_NAME}_C_FLAGS_STR} )
+	set( CMAKE_C_FLAGS ${${PROJECT_NAME}_C_FLAGS_STR} )
 
 	# C++
 	## set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
